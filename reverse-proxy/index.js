6457d005c944fd4e359bfbfe1bcfce1ff41ad1a3
Jeet Das <jeet@das.bz>
2024-05-09 02:36:23 -0500

    Initial commit from transfer

--- reverse-proxy/index.js
+++ reverse-proxy/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const httpProxy = require('http-proxy')
 
 const app = express()
-const PORT = 8000
+const PORT = process.env.PORT || 3000
 
 const BASE_PATH = 'https://cloudbuilder-outputs.s3.us-east-1.amazonaws.com/__outputs'
 
