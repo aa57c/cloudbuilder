0787a224e1209d6ea0be01010ad5a663416a9b02
Jeet Das <jeet@das.bz>
2024-05-09 03:01:00 -0500

    Update

--- reverse-proxy/index.js
+++ reverse-proxy/index.js
@@ -23,4 +23,10 @@ proxy.on('proxyReq', (proxyReq, req, res) => {
         proxyReq.path += 'index.html'
 })
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.listen(PORT, () => console.log(`Reverse Proxy Running..${PORT}`))
