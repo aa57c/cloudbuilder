03fc5bc513ebe6fa9d65d4a349d4d6f2da8e1d3f
Jeet Das <jeet@das.bz>
2024-05-09 01:53:32 -0500

    Updated build

--- frontend-nextjs/app/page.tsx
+++ frontend-nextjs/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
   const handleClickDeploy = useCallback(async () => {
     setLoading(true);
 
-    const { data } = await axios.post(`http://localhost:9000/project`, {
+    const { data } = await axios.post(`http://cloudbuilder.onrender.com/project`, {
       gitURL: repoURL,
       slug: projectId,
     });
