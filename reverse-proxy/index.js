2464b42589cf15a2d3a66b6e1955771cd9673c94
Jeet Das <jeet@das.bz>
2024-05-09 04:22:52 -0500

    Updated build

--- reverse-proxy/index.js
+++ reverse-proxy/index.js
@@ -1,5 +1,5 @@
-const httpProxy = require('http-proxy')
 const express = require('express')
+const httpProxy = require('http-proxy')
 
 const app = express()
 const PORT = process.env.PORT || 9000
@@ -9,24 +9,20 @@ const BASE_PATH = 'https://cloudbuilder-outputs.s3.us-east-1.amazonaws.com/__out
 const proxy = httpProxy.createProxy()
 
 app.use((req, res) => {
-    const pathSegments = req.path.split('/')
-    const subdomain = pathSegments.length > 1 ? pathSegments[1] : ''
+    const hostname = req.hostname;
+    const subdomain = hostname.split('.')[0];
 
     const resolvesTo = `${BASE_PATH}/${subdomain}`
 
     return proxy.web(req, res, { target: resolvesTo, changeOrigin: true })
+
 })
 
 proxy.on('proxyReq', (proxyReq, req, res) => {
     const url = req.url;
     if (url === '/')
         proxyReq.path += 'index.html'
-})
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+})
 
 app.listen(PORT, () => console.log(`Reverse Proxy Running..${PORT}`))
